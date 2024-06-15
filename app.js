08577ec423a1a9ef78b2f27483658a751bffd1d3
Kay <109297172+Blueming9511@users.noreply.github.com>
2024-06-15 13:24:30 +0700

    finish

--- app.js
+++ app.js
@@ -1,34 +1,68 @@
 let boxes_container = document.getElementById('boxes')
 
-for (let index = 1; index <= 9; index++) {
+for (let i = 1; i <= 9; i++) {
   let box = document.createElement('div')
   box.className = 'box align'
-  // box.innerHTML = index
+  box.innerHTML = i
   boxes_container.append(box)
 }
 
 // game
-
 let boxes = document.querySelectorAll('.box')
 let turn = 'X'
 let gameOver = false
 
-boxes.forEach(box => {
-  box.innerHTML = ''
-  box.addEventListener('click', () => {
-    if (!gameOver && box.innerHTML === '') {
-      box.innerHTML = turn
-      changeTurn()
-      checkWinner()
+function innitGame () {
+  boxes.forEach(box => {
+    box.innerHTML = ''
+
+    if (gameOver == false) {
+      box.addEventListener('mouseover', mouseOverFunction)
+      box.addEventListener('mouseout', mouseOutFunction)
+      box.addEventListener('click', clickFunction)
     }
   })
-})
+}
 
+innitGame()
+
+// play again
+document.querySelector('#play-again').addEventListener('click', () => {
+  clearBoxColors();
+  document.querySelector("#result").innerHTML = "";
+  gameOver = false;
+  innitGame();
+  
+})
+function clearBoxColors() {
+  boxes.forEach(box => {
+    box.style.backgroundColor = '';
+  });
+}
+function mouseOverFunction () {
+  if (!gameOver) {
+    this.style.backgroundColor =
+      turn === 'X' ? 'rgb(246, 96, 96)' : ' rgb(78, 137, 189)'
+  }
+}
+
+function mouseOutFunction () {
+  if (!gameOver) {
+    this.style.backgroundColor = '' // Reset background color on mouseout
+  }
+}
+function clickFunction () {
+  if (!gameOver && this.innerHTML === '') {
+    this.innerHTML = turn
+    checkWinner()
+    changeTurn()
+  }
+}
 function changeTurn () {
   if (turn === 'X') {
     turn = 'O'
     document.querySelector('#bg').style.left = '9.8em'
-    document.querySelector('#bg').style.backgroundColor = ' rgb(78, 137, 189)'
+    document.querySelector('#bg').style.backgroundColor = 'rgb(78, 137, 189)'
   } else {
     turn = 'X'
     document.querySelector('#bg').style.left = '5.45em'
@@ -47,4 +81,54 @@ function checkWinner () {
     [0, 4, 8],
     [2, 4, 6]
   ]
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const element = winningCombinations[i]
+    let cellA = boxes[element[0]].innerHTML
+    let cellB = boxes[element[1]].innerHTML
+    let cellC = boxes[element[2]].innerHTML
+
+    // win
+    if (cellA != '' && cellA === cellB && cellA === cellC) {
+      document.querySelector('#result').innerHTML = `${cellA} wins!`
+      gameOver = true
+
+      if (turn == 'X') {
+        for (let j = 0; j < 3; j++) {
+          boxes[winningCombinations[i][j]].style.backgroundColor =
+            'rgb(246, 96, 96)'
+        }
+      } else {
+        for (let j = 0; j < 3; j++) {
+          boxes[winningCombinations[i][j]].style.backgroundColor =
+            'rgb(78, 137, 189)'
+        }
+      }
+    }
+  }
+
+  if (!gameOver) {
+    // checkDraw
+    let draw = true
+    boxes.forEach(box => {
+      if (box.innerHTML === '') {
+        draw = false
+      }
+    })
+
+    if (draw == true) {
+      // draw
+      boxes.forEach(box => {
+        box.style.backgroundColor = ''
+      })
+      document.querySelector('#result').innerHTML = `Draw`
+      gameOver = true
+    }
+  }
 }
+
+// play again
+document.querySelector('#play-again').addEventListener('click', () => {
+  innitGame();
+  
+})
